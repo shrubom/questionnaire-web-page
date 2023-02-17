9a1c02b9299cc094d1f428dde4b5887d71329fed
shrubom <shruti.panicker21@gmail.com>
2023-02-17 15:54:32 +0200

    Added a toggle hide show input field

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let submitButton = document.getElementById("submit-button");
 submitButton.addEventListener("click", function () {
   alert("Thank you for submitting your information!");
 });
+function toggleInput() {
+  var input = document.getElementById("input-field");
+  if (input.style.display === "none") {
+    input.style.display = "block";
+  } else {
+    input.style.display = "none";
+  }
+}
